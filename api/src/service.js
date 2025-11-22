7960ff5e4b3603db33a337d8b9a5fef7cf0f0677
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-11-22 11:17:05 -0300

    adicionando service

---
+++ api/src/service.js
@@ -0,0 +1,33 @@
+
+import dotenv from 'dotenv'
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+dotenv.config()
+
+const genAI = new GoogleGenerativeAI(process.env.KEY)
+
+const obterReceita = async (pergunta) => {
+    try {
+        const systemInstruction = {
+            role: 'user',
+            parts: [{
+                text:"Você é um assistente de IA amigável que vai responder perguntas genéricas"
+            }]
+        }
+
+        const modelResponse = {
+            role:"model",
+            parts:[{
+                text:"olá!"
+            }]
+        }
+
+        const model = genAI.getGenerativeModel({model: "gemini-pro"})
+        const chat = model.startChat({
+            history: [systemInstruction,modelResponse],
+        })
+
+    } catch (error) {
+        
+    }
+}
